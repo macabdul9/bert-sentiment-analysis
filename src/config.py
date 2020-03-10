f0e51890ebab03546c47ead776fbf18a094e76d9
macabdul9 <abdulwaheed1513@gmail.com>
2020-03-10 17:31:47 +0530

    models and training files have been added

--- src/config.py
+++ src/config.py
@@ -1,8 +1,14 @@
+import transformers
+
+
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
-VAL_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 4
 EPOCHS = 10
-ACCUMULATION = 2
-BERT_PATH = "../input/pytorch_model.bin"
-
-
+BERT_PATH = "../input/bert_base_uncased/"
+MODEL_PATH = "pytorch_model.bin"
+TRAINING_FILE = "../input/imdb.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH,
+    do_lower_case=True
+)
